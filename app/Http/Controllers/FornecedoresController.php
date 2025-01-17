990c0babaa335a3726e3487d33179c7ee9090507
kalilfariax <kalilsandes42@gmail.com>
2025-01-17 13:54:35 -0300

    adicionando estilização as views

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class FornecedoresController extends Controller{
-    function fornecedores (){
+    public function fornecedores (){
 
-        return view("site.fornecedores");
+        return view("app.fornecedor.index");
 
     }
 }
---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,7 @@
+<h3>fornecedor</h3>
+
+@php
+   
+
+
+@endphp
