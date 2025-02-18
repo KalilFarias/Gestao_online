d59113a815a85483e84b31eb0cf533ef81375c0e
kalilfariax <kalilsandes42@gmail.com>
2025-02-18 09:19:57 -0300

    migration fornecedores

--- database/migrations/2025_02_12_133956_create_fornecedores_table.php
+++ database/migrations/2025_02_12_133956_create_fornecedores_table.php
@@ -11,15 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('site_contatos', function (Blueprint $table) {
+        Schema::create('fornecedors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name', 50);
-            $table->string('phone', 50);
-            $table->string('email', 80);
-            $table->integer('motivo_contato');
-            $table->text('mensagem');
-            
         });
     }
 
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('site_contatos');
+        Schema::dropIfExists('fornecedors');
     }
 };
