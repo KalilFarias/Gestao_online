b50aa380a5243dbcb8d89c6824c34d63e49d0712
KalilFarias <kalilsandes42@gmail.com>
2025-02-19 14:52:18 -0300

    adição das migrations produtos e alteração da tabela fornecedores

--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model{
 
-    protected $fillable = [
-
-        'nome','phone', 'email', 'motivo_contato', 'mensagem',
-
-    ];
 
 }
